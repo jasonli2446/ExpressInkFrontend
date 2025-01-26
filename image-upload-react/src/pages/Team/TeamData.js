b9e8794dc92a90eb7df0f040c80a3b85b1ff3a9d
Dakin Muhlner <dmuhlner@me.com>
2025-01-25 23:03:39 -0500

    updated team data

--- image-upload-react/src/pages/Team/TeamData.js
+++ image-upload-react/src/pages/Team/TeamData.js
@@ -2,13 +2,13 @@ const teamData = [
     {
       id: 1,
       name: "Aadhav Bharadwaj",
-      bio: "Full stack developer specialized in Node.js",
+      bio: "Back end developer specialized in Node.js",
       image: "/images/aadhav.jpeg", // Replace with actual URLs
     },
     {
       id: 2,
       name: "Dakin Muhlner",
-      bio: "Full stack developer specialized in React.",
+      bio: "Front end developer specialized in React.",
       image:  "/images/dakin.jpeg",
     },
     {
